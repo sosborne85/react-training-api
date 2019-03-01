8a5f1725e19672e8e18a76c0020c913c6fda13a7
Cody Smith <37386486+oncody@users.noreply.github.com>
2019-03-01 13:40:36 -0700

    changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {getCountries} from "./services/populationApi";
+import {getCountries, getCountryYearPopulation} from "./services/populationApi";
 
 class App extends React.Component {
     constructor(props) {
@@ -8,12 +8,14 @@ class App extends React.Component {
 
         this.state = {
             countries: [],
-            countriesLoading: true
+            countriesLoading: true,
+            selectedCountry: '',
+            selectedYear: '',
+            population: 0
         };
     }
 
     componentDidMount() {
-
         getCountries().then((countries) => {
             this.setState({
                 countries,
@@ -22,9 +24,27 @@ class App extends React.Component {
         })
     }
 
+    componentDidUpdate(previousProps, previousState) {
+        if((this.state.selectedCountry === previousState.selectedCountry) && (this.state.selectedYear === previousState.selectedYear)) {
+            return;
+        }
+
+        if(this.state.selectedYear && this.state.selectedCountry) {
+            this.fetchPopulation();
+        }
+    }
+
+    fetchPopulation() {
+        getCountryYearPopulation(this.state.selectedCountry, this.state.selectedYear).then((population) => {
+            this.setState({
+                population
+            });
+        })
+    }
 
     render() {
         const {countries, countriesLoading} = this.state;
+        const years = Array.from({length: (2018 - 1950)}, (v, k) => k + 1950);
         
         return (
             <div className="container">
@@ -33,11 +53,17 @@ class App extends React.Component {
                 <div className="formContainer">
                     <div className="selectContainer">
                         <div className="label">Country:</div>
-                        <select className="select">
+                        <select className="select"
+                                value={this.state.selectedCountry}
+                                onChange={(event) => {
+                                    this.setState({
+                                        selectedCountry: event.target.value
+                                    });
+                                }}>
                             {countriesLoading ? <option>Loading...</option> : (
                                 countries.map(country => {
                                     return (
-                                        <option>{country}</option>
+                                        <option key={country}>{country}</option>
                                     )
                                 })
                             )}
@@ -45,12 +71,26 @@ class App extends React.Component {
                     </div>
                     <div className="selectContainer">
                         <div className="label">Year:</div>
-                        <select className="select"></select>
+                        <select className="select"
+                                value={this.state.selectedYear}
+                                onChange={(event) => {
+                                    this.setState({
+                                        selectedYear: event.target.value
+                                    });
+                                }}>
+                            {countriesLoading ? <option>Loading...</option> : (
+                                years.map(year => {
+                                    return (
+                                        <option key={year}>{year}</option>
+                                    )
+                                })
+                            )}
+                        </select>
                     </div>
                 </div>
 
                 <div className="results">
-                    Results
+                    Results: {this.state.population}
                 </div>
             </div>
         )
