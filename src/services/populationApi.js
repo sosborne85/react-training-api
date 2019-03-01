c867e9053df52b7bbda3ccd2ba4f57043937b7b3
Cody Smith <37386486+oncody@users.noreply.github.com>
2019-03-01 12:56:26 -0700

    Adding in getCountryYearPopluation service

--- src/services/populationApi.js
+++ src/services/populationApi.js
@@ -1,3 +1,30 @@
+export const getCountryYearPopulation = (country, year) => {
+    const url = `http://api.population.io:80/1.0/population/${year}/${country}/`;
+
+    let promise = new Promise((resolve, reject) => {
+        fetch(url, {
+            headers: {
+                accept: 'application/json; charset=utf-8;'
+            }
+        }).then((response) => {
+            return response.json();
+        }).then((data) => {
+            const agePopulationArray = data;
+            let populationSum = 0;
+
+            for(let agePopulation of agePopulationArray) {
+                populationSum += agePopulation.total;
+            }
+
+            resolve(populationSum);
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+
+    return promise;
+}
+
 export const getCountries = () => {
     const url = 'http://api.population.io:80/1.0/countries';
 
